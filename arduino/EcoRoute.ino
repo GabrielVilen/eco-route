69a23951f6d8c71d3acfba100b7de267b3e3b92a
shivanandkohalli <shivanandk58@gmail.com>
2017-11-16 20:31:43 +0100

    Using a  library for GPS parsing

--- arduino/EcoRoute.ino
+++ arduino/EcoRoute.ino
@@ -1,4 +1,4 @@
-#include <DHT.h>
+#include <DHT.h> // For the temperature and humidity sensor 
 
 #define DHTPIN 2
 // Connecting to one of the digital Pins as VCC and ground as, 5 V supply pin not available
@@ -7,19 +7,12 @@
 
 #define ADCREAD_CARBONMONOXIDE 0
 
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h> // For the second serial port
+#include <TinyGPS.h> //GPS Parser
 
 DHT dht(DHTPIN, DHT22);
 
-SoftwareSerial GPS(10, 11); // RX, TX
-int updates;
-int failedUpdates;
-int pos;
-int stringplace = 0;
-
-String timeUp;
-String nmea[15];
-String labels[12] {"Time: ", "Status: ", "Latitude: ", "Hemisphere: ", "Longitude: ", "Hemisphere: ", "Speed: ", "Track Angle: ", "Date: "};
+SoftwareSerial GpsSerail(10, 11); // RX, TX
 
 // This structure will hold all the data to be sent to the Raspberry PI
 struct SensorBundle
@@ -27,9 +20,9 @@ struct SensorBundle
   float humidity;
   float temperature;
   int carbonMonoxideSenVal;
-  String latitude;
+  float latitude;
   String hemisphereLat;
-  String longitude;
+  float longitude;
   String hemisphereLong;
 };
 
@@ -39,7 +32,7 @@ void setup() {
   digitalWrite(5,HIGH);
   digitalWrite(7,LOW);
   Serial.begin(9600);
-  GPS.begin(9600);
+  GpsSerail.begin(9600);
 
   
   dht.begin();
@@ -50,12 +43,7 @@ void loop() {
   SensorBundle sensorBundle;
   bool retVal = false;
     // Wait a few seconds between measurements.
-  getGpsData();
-  // Update the location parameters to be sent
-  sensorBundle.latitude = nmea[2];
-  sensorBundle.hemisphereLat = nmea[3];
-  sensorBundle.longitude = nmea[4];
-  sensorBundle.hemisphereLong = nmea[5];
+  getGpsData(&sensorBundle.latitude, &sensorBundle.longitude);
   
   // Get the Temperature and Humidity data from the sensor
   getTempData(&sensorBundle.temperature, &sensorBundle.humidity);
@@ -71,7 +59,7 @@ void loop() {
   
 }
 
-
+// Send the data to Raspi in JSON format
 void sendDataRasPi(struct SensorBundle *sensorBundle)
 {
   //Serial.println("BEGIN");
@@ -88,16 +76,12 @@ void sendDataRasPi(struct SensorBundle *sensorBundle)
   Serial.print(",");
   
   Serial.print("\"lat\":");
-  sensorBundle->hemisphereLat = "E";
-  String tempString = "\"" +sensorBundle->latitude + " " + sensorBundle->hemisphereLat+ "\"";
-  Serial.print(tempString);
+  Serial.print(sensorBundle->latitude);
   Serial.print(",");
 
 //  Serial.println(sensorBundle->hemisphereLat);
   Serial.print("\"long\":");
-  sensorBundle->hemisphereLong = "N";
-  tempString = "\"" + sensorBundle->longitude + " " +sensorBundle->hemisphereLong + "\"";
-  Serial.print(tempString);
+  Serial.print(sensorBundle->longitude);
   
   Serial.println("}");
   //Serial.println(sensorBundle->hemisphereLong);
@@ -107,14 +91,7 @@ void sendDataRasPi(struct SensorBundle *sensorBundle)
 
 int getCarbonMonData()
 {
-  // int avgCarbonMon = 0;
-  
-  // for(char i=0;i<10;i++)
-  // {
-  //   avgCarbonMon += analogRead(ADCREAD_CARBONMONOXIDE);
-  //   delay(10);
-  // }
-  //return (avgCarbonMon/10);
+
   return analogRead(ADCREAD_CARBONMONOXIDE);
 }
 
@@ -126,106 +103,37 @@ void getTempData(float *temp, float *humidity)
   *temp = dht.readTemperature();
 }
 
-bool getGpsData()
+bool getGpsData(float *latitude, float *longitude)
 {
-  GPS.flush();
-  while (GPS.available() > 0)
-  {
-    GPS.read();
+  TinyGPS gps;
+  bool newLoc;
+  
+  unsigned long chars;
+  unsigned short sentences, failed;
 
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (GpsSerail.available())
+    {
+      char c = GpsSerail.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newLoc = true;
+    }
+  }
+
+  if (newLoc)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    
+    *latitude = flat;
+    *longitude = flon;
+    
   }
   
-  // To get to know the number of satellites in view
-  if(GPS.find("$GPGSV"))
-  {
-    String temp = GPS.readStringUntil('\n');
-    Serial.print("[DEBUG]:"); 
-    Serial.println(temp);
-  }
-  
-  if (GPS.find("$GPRMC,")) {
-    String temp = GPS.readStringUntil('\n');
-    for (int i = 0; i < temp.length(); i++) {
-      if (temp.substring(i, i + 1) == ",") {
-        nmea[pos] = temp.substring(stringplace, i);
-        stringplace = i + 1;
-        pos++;
-      }
-      if (i == temp.length() - 1) {
-        nmea[pos] = temp.substring(stringplace, i);
-      }
-    }
-    nmea[2] = ConvertLat();
-    nmea[4] = ConvertLng();
-    for (int i = 0; i < 9; i++) {
-      //Serial.print(labels[i]);
-      //Serial.print(nmea[i]);
-      //Serial.println("");
-    }
-    stringplace = 0;
-    pos = 0;
-    return true;
-  
-  }
-  return false;
-
-}
-
-String ConvertLat() {
-  String posneg = "";
-  if (nmea[3] == "S") {
-    posneg = "-";
-  }
-  String latfirst;
-  float latsecond;
-  for (int i = 0; i < nmea[2].length(); i++) {
-    if (nmea[2].substring(i, i + 1) == ".") {
-      latfirst = nmea[2].substring(0, i - 2);
-      latsecond = nmea[2].substring(i - 2).toFloat();
-    }
-  }
-  latsecond = latsecond / 60;
-  String CalcLat = "";
-
-  char charVal[9];
-  dtostrf(latsecond, 4, 6, charVal);
-  for (int i = 0; i < sizeof(charVal); i++)
-  {
-    CalcLat += charVal[i];
-  }
-  latfirst += CalcLat.substring(1);
-  latfirst = posneg += latfirst;
-  return latfirst;
-}
-
-String ConvertLng() {
-  String posneg = "";
-  if (nmea[5] == "W") {
-    posneg = "-";
-  }
-
-  String lngfirst;
-  float lngsecond;
-  for (int i = 0; i < nmea[4].length(); i++) {
-    if (nmea[4].substring(i, i + 1) == ".") {
-      lngfirst = nmea[4].substring(0, i - 2);
-      //Serial.println(lngfirst);
-      lngsecond = nmea[4].substring(i - 2).toFloat();
-      //Serial.println(lngsecond);
-
-    }
-  }
-  lngsecond = lngsecond / 60;
-  String CalcLng = "";
-  char charVal[9];
-  dtostrf(lngsecond, 4, 6, charVal);
-  for (int i = 0; i < sizeof(charVal); i++)
-  {
-    CalcLng += charVal[i];
-  }
-  lngfirst += CalcLng.substring(1);
-  lngfirst = posneg += lngfirst;
-  return lngfirst;
+  gps.stats(&chars, &sentences, &failed);
 }
 
 
